1826c6142507aaa1c2f1b2e8672b23cf1fa19944
fxmayl <1318847212@qq.com>
2023-02-12 12:31:49 +0800

    滑动窗口

--- src/SlidingWindow.java
+++ src/SlidingWindow.java
@@ -14,6 +14,11 @@ public class SlidingWindow {
         System.out.println(minCoverSubSeq("EBBANCF", "ABC"));
     }
 
+    @Test
+    public void test2() {
+        System.out.println(checkIn("hellowtorld", "oow"));
+    }
+
     /**
      * 最小覆盖字串
      * <p>
@@ -65,5 +70,55 @@ public class SlidingWindow {
             }
         }
         return len == Integer.MAX_VALUE ? "" : s.substring(start, start + len);
+    }
+
+
+    /**
+     * 校验字符串s中是否包含t中的所有字母，且不含邮其他的字母
+     *
+     * @return
+     */
+    private boolean checkIn(String s, String t) {
+        Map<Character, Integer> needs = new HashMap<>();
+        char[] tChars = t.toCharArray();
+        for (int i = 0; i < tChars.length; i++) {
+            needs.put(tChars[i], needs.getOrDefault(tChars[i], 0) + 1);
+        }
+
+        Map<Character, Integer> window = new HashMap<>();
+        int right = 0;
+        int left = 0;
+        // 用于表示字符串t中字母的个数
+        int valid = 0;
+        while (right < s.length()) {
+            char c = s.charAt(right);
+            right++;
+            if (needs.containsKey(c)) {
+                window.put(c, window.getOrDefault(c, 0) + 1);
+                // 表示窗口中的字母 c 满足的条件
+                if (window.get(c).equals(needs.get(c))) {
+                    valid++;
+                }
+            }
+            // right - left >= t.length() 表示 窗口的大小始终只有 t.length() 这个大小
+            // 因为 当valid 一直不合法时， 窗口左侧会一直缩小
+            while (right - left >= t.length()) {
+                // 判断是否找到了合法的
+                if (valid == needs.size()) {
+                    return true;
+                }
+                // 移动窗口左边
+                char leftChar = s.charAt(left);
+                left++;
+                if (needs.containsKey(leftChar)) {
+                    // 如果leftChar的数量与 t 中一致，则在缩短左边窗口的时候，valid就不满足，需要减去1
+                    if (window.get(leftChar).equals(needs.get(leftChar))) {
+                        valid--;
+                    }
+                    window.put(leftChar, window.get(leftChar) - 1);
+                }
+            }
+        }
+        return false;
     }
 }
