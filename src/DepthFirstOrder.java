57bc7512441fec363d18e4405d1de7b58451055c
fxmayl <1318847212@qq.com>
2023-10-10 15:17:39 +0800

    有向图中基于深度优先搜索的顶点排序

--- src/DepthFirstOrder.java
+++ src/DepthFirstOrder.java
@@ -37,4 +37,18 @@ public class DepthFirstOrder {
         post.offer(v);
         reversePost.push(v);
     }
+
+    public static void main(String[] args) {
+        Digraph digraph = new Digraph(4);
+        digraph.addEdge(0, 1);
+        digraph.addEdge(1, 2);
+        digraph.addEdge(1, 3);
+        DepthFirstOrder depthFirstOrder = new DepthFirstOrder(digraph);
+        System.out.println(depthFirstOrder.pre);
+        System.out.println(depthFirstOrder.post);
+//        System.out.println(depthFirstOrder.reversePost);
+        for (int i = 0; !depthFirstOrder.reversePost.isEmpty(); i++) {
+            System.out.println(depthFirstOrder.reversePost.pop());
+        }
+    }
 }
